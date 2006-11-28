fee391e473aa6ae7c189a4d134e2d46eb6afd650
root66 <root66@1261d71b-3423-0410-b5c4-9d10f2b9fcb2>
2006-11-28 21:38:28 +0000

    Added preliminary script support, improved error checking

--- applications/GhettoSL/ghetto.cs
+++ applications/GhettoSL/ghetto.cs
@@ -965,13 +965,22 @@ namespace ghetto
             {
                 char[] splitChar = { ' ' };
                 string[] args = input.ToLower().Split(splitChar);
-                string[] commands = { "goto", "label", "sit", "stand", "touch", "touchid", "wait" };
-                if (Array.IndexOf(commands,  args[0]) > -1)
+                string[] commandsWithArgs = { "goto", "label", "say", "shout", "sit", "touch", "touchid", "wait", "whisper" };
+                string[] commandsWithoutArgs = { "quit", "relog", "sitg", "stand" };
+                if (Array.IndexOf(commandsWithArgs,  args[0]) > -1)
                 {
-                    Array.Resize(ref script, i + 1);
-                    script[i] = input;
+                    if (args.Length < 2)
+                    {
+                        Console.WriteLine("Missing argument(s) for command \"{0}\" on line {1} of {2}",args[0],i+1,scriptFile);
+                        error++;
+                    }
+                    else
+                    {
+                        Array.Resize(ref script, i + 1);
+                        script[i] = input;
+                    }
                 }
-                else
+                else if (Array.IndexOf(commandsWithoutArgs, args[0]) < 0)
                 {
                     Console.WriteLine("Unknown command \"{0}\" on line {1} of {2}",args[0],i+1,scriptFile);
                     error++;
@@ -993,7 +1002,33 @@ namespace ghetto
 
         void RunScript(string[] script)
         {
-
+            for (int i = 0; i < script.Length; i++)
+            {
+                char[] splitChar = { ' ' };
+                string[] cmd = script[i].Split(splitChar);
+                switch (cmd[0])
+                {
+                    case "wait":
+                        {
+                            Console.WriteLine("* Sleeping {0} seconds...", cmd[1]);
+                            Thread.Sleep(int.Parse(cmd[1]) * 1000);
+                            continue;
+                        }
+                    case "goto":
+                        {
+                            int findLabel = Array.IndexOf(script, "label " + cmd[1]);
+                            if (findLabel > -1) i = findLabel;
+                            else Console.WriteLine("* Label \"{0}\" not found on line {1}", cmd[1], i+1);
+                            continue;
+                        }
+                    case "label":
+                        {
+                            continue;
+                        }
+                }
+                Console.WriteLine("* SCRIPTED COMMAND: "+script[i]);
+                ParseCommand(true, "/"+script[i], "", new LLUUID(), new LLUUID());
+            }
         }
 
 
