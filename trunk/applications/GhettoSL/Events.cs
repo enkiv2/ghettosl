ca6e51efcf4662415f04ad59e123f476927d344d
root66 <root66@1261d71b-3423-0410-b5c4-9d10f2b9fcb2>
2006-12-15 05:25:47 +0000

    Finished categorizing functions and did some housecleaning

--- trunk/applications/GhettoSL/Events.cs
+++ trunk/applications/GhettoSL/Events.cs
@@ -1,3 +1,31 @@
+/*
+* Copyright (c) 2006, obsoleet industries
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of obsoleet industries nor the names of its
+*       contributors may be used to endorse or promote products derived from
+*       this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -241,6 +269,98 @@ namespace ghetto
                 }
             }
         }
+
+
+        void OnFetchInventoryReply(Packet packet, Simulator sim)
+        {
+            //fetch inventory item data - FIXME!!!
+            FetchInventoryReplyPacket reply = (FetchInventoryReplyPacket)packet;
+            FetchInventoryReplyPacket.InventoryDataBlock obj = new FetchInventoryReplyPacket.InventoryDataBlock();
+
+            Console.WriteLine("Inventory info: " + obj.Name);
+
+            //rez object from inventory - FIXME!!!
+            RezObjectPacket p = new RezObjectPacket();
+            p.AgentData.AgentID = Client.Network.AgentID;
+            p.AgentData.SessionID = Client.Network.SessionID;
+            p.InventoryData.BaseMask = obj.BaseMask;
+            p.InventoryData.CRC = obj.CRC;
+            p.InventoryData.CreationDate = obj.CreationDate;
+            p.InventoryData.CreatorID = obj.CreatorID;
+            p.InventoryData.Description = obj.Description;
+            p.InventoryData.EveryoneMask = obj.EveryoneMask;
+            p.InventoryData.Flags = obj.Flags;
+            p.InventoryData.FolderID = obj.FolderID;
+            p.InventoryData.GroupID = obj.GroupID;
+            p.InventoryData.GroupMask = obj.GroupMask;
+            p.InventoryData.GroupOwned = obj.GroupOwned;
+            p.InventoryData.InvType = obj.InvType;
+            p.InventoryData.ItemID = obj.ItemID;
+            p.InventoryData.Name = obj.Name;
+            p.InventoryData.NextOwnerMask = obj.NextOwnerMask;
+            p.InventoryData.OwnerID = obj.OwnerID;
+            p.InventoryData.OwnerMask = obj.OwnerMask;
+            p.InventoryData.SalePrice = obj.SalePrice;
+            p.InventoryData.SaleType = obj.SaleType;
+            //p.InventoryData.TransactionID = ?
+            p.InventoryData.Type = obj.Type;
+
+            LLVector3 rezPos = Client.Self.Position;
+            rezPos.X++;
+            rezPos.Y++;
+            p.RezData.RayEnd = rezPos;
+            p.RezData.RayStart = Client.Self.Position;
+            p.RezData.RayEndIsIntersection = false;
+            p.RezData.RezSelected = false;
+            p.RezData.RemoveItem = false;
+            p.RezData.BypassRaycast = 1;
+
+            //Client.Network.SendPacket(p);
+
+        }
+
+
+
+        void OnObjectSelect(Packet packet, Simulator sim)
+        {
+            ObjectSelectPacket reply = (ObjectSelectPacket)packet;
+            //FIXME -- DETECT WHAT masterID IS GRABBING
+            //if (reply.AgentData.AgentID == masterID)
+            //{
+            Console.WriteLine("* Touched/grabbed object " + reply.ObjectData[0].ObjectLocalID);
+            //}
+        }
+
+        //FRIEND REQUESTS (FIXME!!!) #########################################
+        void OnRequestFriendship(Packet packet, Simulator simulator)
+        {
+            RequestFriendshipPacket reply = (RequestFriendshipPacket)packet;
+            if (reply.AgentData.AgentID != masterID) return;
+            AcceptFriendshipPacket p = new AcceptFriendshipPacket();
+            p.AgentData.AgentID = Client.Network.AgentID;
+            p.AgentData.SessionID = Client.Network.SessionID;
+            p.TransactionBlock.TransactionID = reply.AgentBlock.TransactionID;
+            AcceptFriendshipPacket.FolderDataBlock[] folder = new AcceptFriendshipPacket.FolderDataBlock[1];
+            folder[0].FolderID = reply.AgentBlock.FolderID;
+            p.FolderData = folder;
+            Client.Network.SendPacket(p);
+        }
+        //END OF FRIEND REQUESTS ##############################################
+
+
+        //AUTO-CAMP OBJECT-FINDING STUFF ######################################
+        void OnObjectUpdateEvent(Packet packet, Simulator sim)
+        {
+            ObjectUpdatePacket p = (ObjectUpdatePacket)packet;
+            foreach (ObjectUpdatePacket.ObjectDataBlock obj in p.ObjectData)
+            {
+                //FIXME!!! Update prim text
+                //if (prims[obj.ID])
+                //    prims[obj.ID].Text = Helpers.FieldToString(obj.Text);
+            }
+        }
+        //END OF AUTO-CAMP STUFF ##############################################
+
 
 
     }
