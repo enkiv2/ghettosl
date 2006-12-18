23de63ed5d6eddc0ce9279c4a0a880905b12c917
root66 <root66@1261d71b-3423-0410-b5c4-9d10f2b9fcb2>
2006-12-18 20:39:51 +0000

    Improved help system

--- applications/GhettoSL/Help.cs
+++ applications/GhettoSL/Help.cs
@@ -8,47 +8,65 @@ namespace ghetto
     {
         public void Help(string helpTopic)
         {
-            string topic = "";
-            if (helpTopic != null) topic = helpTopic.ToLower();
-            switch (topic)
-            {
-                case "":
-                    {
-                        Console.WriteLine(
-                            "--[ Commands ]--------------------------------------------------------\r\n" +
-                            "anim <uuid> \t\tStart the specified animation\r\n" +
-                            "camp <text> \t\tFind a chair with text matching the specified string\r\n" +
-                            "clear \t\t\tClear the console display\r\n" +
-                            "die \t\t\tLog out and exits\r\n" +
-                            "fly \t\t\tEnable flying\r\n" +
-                            "follow <name|off> \tFollow the specified avatar, or \"off\" to disable\r\n" +
-                            "land \t\t\tDisable flying\r\n" +
-                            "listen \t\t\tListen to local chat (on by default)\r\n" +
-                            "quiet \t\t\tStop listening to local chat\r\n" +
-                            "re [# message] \t\tList active IM \"windows\" or reply by window ID\r\n" +
-                            "relog \t\t\tLog out and back in\r\n" +
-                            "ride <name> \t\tSit on the same object as the specified name\r\n" +
-                            "run \t\t\tEnable running\r\n" +
-                            "script <scriptName> \tExecute the specified script file\r\n" +
-                            "shout <message> \tShout the specified message to users within 100m\r\n" +
-                            "sit <uuid> \t\tSit on the specified UUID\r\n" +
-                            "sitg \t\t\tSit on the ground at current location\r\n" +
-                            "stand \t\t\tStand while seated on an object or on the ground\r\n" +
-                            "stopanim <uuid> \tStop the specified animation\r\n" +
-                            "teleport <sim> [x y z] \tTeleports to the specified destination\r\n" +
-                            "time \t\t\tDisplays time and region sun direction\r\n" +
-                            "touch <uuid> \t\tTouch the specified object\r\n" +
-                            "touchid <localID> \tTouch the specified object LocalID\r\n" +
-                            "walk \t\t\tDisable running\r\n" +
-                            "whisper <message> \tWhisper the specified message to users within 5m\r\n" +
-                            "who \t\t\tList avatars within viewing range\r\n" +
-                            "----------------------------------------------------------------------\r\n"
-                        );
-                        return;
-                    }
-            }
+            
 
-            Console.WriteLine("No help available for that topic. Type /help for a list of commands.");
+            Dictionary<string,string> HelpDict = new Dictionary<string, string>();
+            HelpDict.Add("anim <uuid>", "Start the specified animation");
+            HelpDict.Add("camp <text>", "Find a chair with text matching the specified string");
+            HelpDict.Add("clear", "Clear the console display");
+            HelpDict.Add("die", "Log out and exit");
+            HelpDict.Add("fly", "Enable flying");
+            HelpDict.Add("follow <name|off>", "Follow the specified avatar, or \"off\" to disable");
+            HelpDict.Add("land", "Disable flying");
+            HelpDict.Add("listen", "Listen to local chat (on by default)");
+            HelpDict.Add("quiet", "Stop listening to local chat");
+            HelpDict.Add("re [# message]", "List active IM \"windows\" or reply by window ID");
+            HelpDict.Add("relog", "Log out and back in");
+            HelpDict.Add("ride <name>", "Sit on the same object as the specified name");
+            HelpDict.Add("run", "Enable running");
+            HelpDict.Add("script <scriptName>", "Execute the specified script file");
+            HelpDict.Add("shout <message>", "Shout the specified message to users within 100m");
+            HelpDict.Add("sit <uuid>", "Sit on the specified UUID");
+            HelpDict.Add("sitg", "Sit on the ground at current location");
+            HelpDict.Add("stand", "Stand while seated on an object or on the ground");
+            HelpDict.Add("stopanim <uuid>", "Stop the specified animation");
+            HelpDict.Add("teleport <sim> [x y z]", "Teleports to the specified destination");
+            HelpDict.Add("time", "Displays time and region sun direction");
+            HelpDict.Add("touch <uuid>", "Touch the specified object");
+            HelpDict.Add("touchid <localID>", "Touch the specified object LocalID");
+            HelpDict.Add("walk", "Disable running");
+            HelpDict.Add("whisper", "Whisper the specified message to users within 5m");
+            HelpDict.Add("who", "List avatars within viewing range");
+
+            string topic = "";
+            string result = "";
+            if (helpTopic != null) topic = helpTopic.ToLower();
+
+            if (topic == "")
+            {
+                WinConsole.Color = ConsoleColor.Cyan; WinConsole.Write("\r\n-=");
+                WinConsole.Color = ConsoleColor.Cyan | ConsoleColor.Intensified; WinConsole.Write("[");
+                WinConsole.Color = ConsoleColor.White | ConsoleColor.Intensified; WinConsole.Write(" Commands ");
+                WinConsole.Color = ConsoleColor.Cyan | ConsoleColor.Intensified; WinConsole.Write("]");
+                WinConsole.Color = ConsoleColor.Cyan; WinConsole.Write("=-------------------------------------------------------\r\n");
+                WinConsole.Color = ConsoleColor.White;
+
+                foreach (KeyValuePair<string, string> pair in HelpDict)
+                {
+                    string spaces = "";
+                    for (int sp = pair.Key.Length; sp < 24; sp++) spaces += " ";
+                    WinConsole.Color = ConsoleColor.White | ConsoleColor.Intensified;
+                    WinConsole.Write(pair.Key + spaces);
+                    WinConsole.Color = ConsoleColor.White;
+                    WinConsole.Write(pair.Value + "\r\n");
+                }
+
+                WinConsole.Color = ConsoleColor.Cyan;
+                WinConsole.WriteLine("----------------------------------------------------------------------\r\n");
+                WinConsole.Color = ConsoleColor.White;
+            }
+            else if (HelpDict.TryGetValue(topic, out result)) Console.WriteLine(topic + " - " + result);
+            else Console.WriteLine("No help available for that topic. Type /help for a list of commands.");
 
         }
     }
