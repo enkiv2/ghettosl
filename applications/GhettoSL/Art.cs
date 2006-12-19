6d27296b20456adc289d4fdb1e1805795ded06e4
root66 <root66@1261d71b-3423-0410-b5c4-9d10f2b9fcb2>
2006-12-19 00:16:36 +0000

    Fixed masterID not being optional Added Art.cs and new intro art

---
+++ applications/GhettoSL/Art.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ghetto
+{
+    partial class GhettoSL
+    {
+        void IntroArt()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\r\n================================================================");
+            Console.WriteLine("================================================================");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("           __                             ________              ");
+            Console.WriteLine("          |  |                           /        \\             ");
+            Console.WriteLine("          |  |                          |   ____   |__          ");
+            Console.WriteLine("  ________|  |                          |  |    |__|  |         ");
+            Console.WriteLine(" |   ___   | |    _______  _            |  |      |   |         ");
+            Console.WriteLine(" |  |   |  | +___|       || |___    ____|_ +______|   |         ");
+            Console.WriteLine(" |  |   |  |      | ___  |.  ___|. |      |        |  |         ");
+            Console.WriteLine(" |  +___|  |  __  ||   | || |__| |_|   .  |_____   |  |         ");
+            Console.WriteLine(" |______   | |  | |+___| || |__   __| ||  |     |  |  |         ");
+            Console.WriteLine("  _     |  | |  | | _____|| |  | | |  ||  |.____|  |  |_______  ");
+            Console.WriteLine(" | |____|  | |  | ||_____ | |  | | |  `   |        |          | ");
+            Console.WriteLine(" |_________|_|  |_|______||_|  |_| |______|_______/+__________| ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("                                   (c) 2006 obsoleet industries ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("================================================================");
+            Console.WriteLine("================================================================\r\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        void HeaderHelp()
+        {
+            Console.ForegroundColor = System.ConsoleColor.DarkCyan; Console.Write("\r\n-=");
+            Console.ForegroundColor = System.ConsoleColor.Cyan; Console.Write("[");
+            Console.ForegroundColor = System.ConsoleColor.White; Console.Write(" Commands ");
+            Console.ForegroundColor = System.ConsoleColor.Cyan; Console.Write("]");
+            Console.ForegroundColor = System.ConsoleColor.DarkCyan; Console.Write("=----------------------------------------------------------------\r\n");
+            Console.ForegroundColor = System.ConsoleColor.Gray;
+        }
+
+        void HeaderWho()
+        {
+            Console.ForegroundColor = System.ConsoleColor.DarkCyan; Console.Write("\r\n-=");
+            Console.ForegroundColor = System.ConsoleColor.Cyan; Console.Write("[");
+            Console.ForegroundColor = System.ConsoleColor.White; Console.Write(" Nearby Avatars ");
+            Console.ForegroundColor = System.ConsoleColor.Cyan; Console.Write("]");
+            Console.ForegroundColor = System.ConsoleColor.DarkCyan; Console.Write("=----------------------------------------------------------\r\n");
+            Console.ForegroundColor = System.ConsoleColor.Gray;
+        }
+        void Footer()
+        {
+            Console.ForegroundColor = System.ConsoleColor.DarkCyan;
+            Console.WriteLine("-------------------------------------------------------------------------------\r\n");
+            Console.ForegroundColor = System.ConsoleColor.Gray;
+        }
+    }
+}
